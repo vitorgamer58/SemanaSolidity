153243eee116ad79645a66ee2d2823b736c5c283
Vitor Araujo <vitorgamer58@outlook.com>
2022-02-28 18:02:52 -0300

    Aula 4

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,10 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0 <0.8.0;
+pragma solidity ^0.8.1;
 
 contract HelloWorld {
     string public message = "Ola Semana Solidity!";
 
-    function greetings() public view returns (string memory) {
-        return message;
+    string public constant MESSAGE_CONSTANT = "Ola constante!";
+
+    address public constant DONATION_ADDRESS =
+        0x918af765D9AF1aF74B3F7b0F460D271580d75deB;
+
+    address public immutable OWNER;
+
+    constructor () {
+        OWNER = msg.sender;
+    }
+
+    function greetings() public pure returns (string memory) {
+        string memory internalmessage = "Ola Semana Solidity!";
+
+        return internalmessage;
+    }
+
+    function getBlockNumber() public view returns (uint256) {
+        return block.timestamp;
     }
 }
